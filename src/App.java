01e84b307d991c4516d15821e0f770b7e7a7bcdc
Joren <babamyloves2002@gmail.com>
2023-01-22 22:58:25 -0800

    added factorial codes

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       int num = factorial(5);
+       System.out.println(num);
     }
+
+    public static int factorial(int x) {
+        if(x == 1) {
+            return x;
+        }
+        else {
+            return x * factorial(x - 1);
+        }
+    }
+
 }
